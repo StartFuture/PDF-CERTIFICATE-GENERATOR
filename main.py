3185d8bde37bd173d644e8ec77977cccee114be3
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2022-07-04 23:33:05 -0300

    Test only if it's main file

--- main.py
+++ main.py
@@ -28,29 +28,30 @@ Extras:
 [ ] Func to make human Readable 
 '''
 
+if __name__ == '__main__':
 
-path_output = 'output'
-filename_output = 'report.pdf'
-path_template = 'templates'
-filename_template = 'clear_template.pdf'
-value = "Valor Teste"
+    path_output = 'output'
+    filename_output = 'report.pdf'
+    path_template = 'templates'
+    filename_template = 'clear_template.pdf'
+    value = "Valor Teste"
 
-path_input_json = 'input'
-filename_input_json = 'value_01'
+    path_input_json = 'input'
+    filename_input_json = 'value_01'
 
 
-list_infos_json = functions.read_input_json(path=path_input_json, filename=filename_input_json)
-# list_infos_csv = functions.read_input_csv(path=path_input_json, filename=filename_input_json)
-# list_infos_xlsx = functions.read_input_excel(path=path_input_json, filename=filename_input_json)
+    list_infos_json = functions.read_input_json(path=path_input_json, filename=filename_input_json)
+    # list_infos_csv = functions.read_input_csv(path=path_input_json, filename=filename_input_json)
+    # list_infos_xlsx = functions.read_input_excel(path=path_input_json, filename=filename_input_json)
 
-output = PdfFileWriter()
-for dict_infos in list_infos_json:
-    
-    dict_template_mail = functions.process_dict_template(dict_json_input=dict_infos)
+    output = PdfFileWriter()
+    for dict_infos in list_infos_json:
+        
+        dict_template_mail = functions.process_dict_template(dict_json_input=dict_infos)
 
-    output = functions.generate_direct_mail(path_template=path_template, filename_template=filename_template, dict_values=dict_template_mail, path_input_json=path_input_json, filename_input_json=filename_input_json, output=output)
-    
-functions.save_fill_template(path=path_output, filename=filename_output, output=output)
+        output = functions.generate_direct_mail(path_template=path_template, filename_template=filename_template, dict_values=dict_template_mail, path_input_json=path_input_json, filename_input_json=filename_input_json, output=output)
+        
+    functions.save_fill_template(path=path_output, filename=filename_output, output=output)
 
 
 
